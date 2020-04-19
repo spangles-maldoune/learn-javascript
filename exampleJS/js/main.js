d1e97d10a75dbaa481d6fa7840f078fd9b37be1d
spangles-maldoune <61394048+spangles-maldoune@users.noreply.github.com>
2020-04-19 21:43:26 +0300

    upload in dev branch

---
+++ exampleJS/js/main.js
@@ -0,0 +1,17 @@
+
+let num = prompt (' ');
+switch (num) { 	 		//название переменной
+	case '1': 						//первое условие при значении 1
+		result = ('Зима'); //если все переменные result написать с ключевым словом let, то не работает
+	break;
+	case '2':
+		result = ('Весна'); 
+	break;
+	case '3':
+		result = ('Лето');
+	break;
+	case '4':
+		result = ('Осень');
+	break;
+};
+alert (result);
