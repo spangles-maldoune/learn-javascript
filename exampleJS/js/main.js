dae60f345dddf7cdaf3059d86fcd6f444137eaff
Roman Andriianov <randriyanov9@gmail.com>
2020-04-20 22:30:17 +0300

    added solution with more than 6 elements

--- exampleJS/js/main.js
+++ exampleJS/js/main.js
@@ -1,4 +1,3 @@
-
 // let num = prompt (' ');
 // let result;
 // switch (num) { 	 		//название переменной
@@ -6,7 +5,7 @@
 // 		result = ('Зима'); //если все переменные result написать с ключевым словом let, то не работает
 // 	break;
 // 	case '2':
-// 		result = ('Весна'); 
+// 		result = ('Весна');
 // 	break;
 // 	case '3':
 // 		result = ('Лето');
@@ -32,12 +31,12 @@
 
 // let str = 'abcde';
 // if (str[0] == 'a') {
-// 	alert ('Da!'); 
+// 	alert ('Da!');
 // } else alert ('Net!');
 
 // let str = '12345';
 // if (str[0] == '1' || str[0] == '2' || str[0] == '3') {
-// 	alert ('Da!'); 
+// 	alert ('Da!');
 // } else alert ('Net!');
 
 
@@ -54,3 +53,10 @@ if (stValL == stValR) {
 
 console.log(stValL);
 console.log(stValR);
+
+//this is solution if you need to compare more than 6 elements
+let stVal = '12344321';
+let sum = (input) => input.split('').map(element => +element).reduce((basic, input) => basic + input)
+let stValL = sum(stVal.substring(0, stVal.length / 2));
+let stValR = sum(stVal.substring(stVal.length / 2, stVal.length));
+console.log(`${stValL === stValR ? 'Da!!!' : 'Net!!!'}`)
