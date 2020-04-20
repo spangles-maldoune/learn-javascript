f14c1558c4f565000b45f2c22eb5367562afc276
spangles-maldoune <61394048+spangles-maldoune@users.noreply.github.com>
2020-04-20 15:25:26 +0300

    corrections made

--- 1.2/js/main.js
+++ 1.2/js/main.js
@@ -1,6 +1,6 @@
-let user1 = '18 лет Юрий Иванов';
-let user2 = '16 лет Дмитрий Кузьмин';
-let user3 = '19 лет Оксана Петрова';
+let user1 = prompt('Введите ваши данные (возраст, имя и фамилию):', '18 лет Юрий Иванов');
+let user2 = prompt('Введите ваши данные (возраст, имя и фамилию):', '16 лет Дмитрий Кузьмин');
+let user3 = prompt('Введите ваши данные (возраст, имя и фамилию):', '19 лет Оксана Петрова');
 let averageAge = (parseInt (user1) + parseInt (user2) + parseInt (user3)) / 3;
 
 console.log ('**************');
@@ -13,6 +13,6 @@ console.log ('2. ', user2);
 console.log (' ');
 console.log ('3. ', user3);
 console.log (' ');
-console.log ('-------------- Средний возраст студента: ' + (+averageAge.toFixed(1)) + ' --------------');
+console.log ('-------------- Средний возраст студента: ' + (+averageAge.toFixed(2)) + ' --------------');
 console.log (' ');
 console.log('**************');
