92b523f315f97c3bc308068ccb658bd0adacfb41
Roman Andriianov <randriyanov9@gmail.com>
2020-05-10 20:48:02 +0300

    added solution with array sum

--- new/js/mainObj.js
+++ new/js/mainObj.js
@@ -1,52 +1,57 @@
 // Написать функцию заполнения массива.Имя произвольное.
 
 function arrMake(min, max, size) {
-	var arr = [];
-	for (var i = 0; i < size; i++) {
-		arr[i] = Math.floor(Math.random() * (max - min) + min);
-	}
-	return arr;
+    var arr = [];
+    for (var i = 0; i < size; i++) {
+        arr[i] = Math.floor(Math.random() * (max - min) + min);
+    }
+    return arr;
 }
 
-var arr1 = arrMake (0, 10, 10);
+var arr1 = arrMake(0, 10, 10);
 var arr2 = arrMake(0, 10, 10);
-console.log (arr1);
-console.log (arr2);
+console.log(arr1);
+console.log(arr2);
 
 
 // написать функцию, которая примет как аргумент(параметр) два массива и сравнить суммы всех ЧИСЛОВЫХ элементов.
 // Тот массив, сумма которого большая - должен вернутся функцией.
 
-function arrReturn (arr) {
-	var summ = 0;
-	for (var i = 0; i < arr.length; i++) {
-		summ += arr[i]
-	}
-	console.log (summ);
+function arrReturn(arr) {
+    var summ = 0;
+    for (var i = 0; i < arr.length; i++) {
+        summ += arr[i]
+    }
+    console.log(summ);
 }
 
 arrReturn(arr1);
 
+console.log("Task 1")
 
+function sumTwoArray(arr1, arr2) {
+    var longestIndex = arr1.length;
+    if (longestIndex < arr2.length) {
+        longestIndex = arr2.length;
+    }
+    var sumArrayOne = 0;
+    var sumArrayTwo = 0;
+    for (var i = 0; i < longestIndex; i++) {
+        if (+arr1[i]) {
+            sumArrayOne += arr1[i]
+        }
+        if (+arr2[i]) {
+            sumArrayTwo += arr2[i]
+        }
+    }
+    if (sumArrayOne > sumArrayTwo) {
+        return arr1
+    } else {
+        return arr2
+    }
 
-function arrReturn (arr1, arr2) {
-	var mass = [arr1, arr2];
-	var summ = [0, 0];
-	for (var i = 0; i < mass.length; i++) {
-		for (var j = 0; j < mass[i].length; j++) {
-			if (typeof (mass[i][j]) === 'number') {
-				summ[i] = summ[i] + mass[i][j];
-			} else {
-				continue;
-			}
-
-		}
-
-	}
-	
-	console.log (summ, 'summ');
 }
 
-var mass = [1, 2, 3];
+var mass = ["test", 2, 3];
 var mass2 = [2, 3, 2];
-arrReturn(mass, mass2);
+sumTwoArray(mass, mass2);
