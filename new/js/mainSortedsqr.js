26bbf5e2d0adcba634c5d7d4d19807869b7f0be5
Roman Andriianov <randriyanov9@gmail.com>
2020-05-16 19:47:57 +0300

    some code cleanup

--- new/js/mainSortedsqr.js
+++ new/js/mainSortedsqr.js
@@ -3,17 +3,17 @@ console.log(arr);
 let temp;
 
 for (let i = 0; i < arr.length; i++) {
-	arr[i] *= arr[i];
+    arr[i] *= arr[i];
 }
 
 for (let i = 0; i < arr.length; i++) {
-	for (let j = 0; j < arr.length; j++) {
-		if (arr[j] > arr[i]) {
-			temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
-		}
-	}
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j] > arr[i]) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
 }
 
 console.log(arr);
@@ -26,36 +26,36 @@ console.log(arr);
 // в случае установки значения n которое не прошло валидацию выдать в консоль ошибку.
 
 function initSlider(size) {
-   return {
-      count: 0,
-      up: function () {
-         if (this.count === size) {
-            this.count = 0;
-            return this.count;
-         }
-         ++this.count;
-         console.log (this.count);
-      },
-      down: function () {
-         if (this.count === 0) {
-            this.count = size;
-            return this.count;
-         }
-         --this.count;
-         console.log (this.count);
-      },
-      set: function (n) {
-         if (n > 0 && n <= size) {
-            this.count = n;
-            console.log (this.count);
-         } else {
-            console.log('Error!!!')
-         }
-      },
-      reset: function () {
-         console.log (this.count = 0);
-      } 
-   }
+    let count = 0;
+    return {
+        up: function () {
+            if (count === size) {
+                count = 0;
+                return count;
+            }
+            ++count;
+            console.log(count);
+        },
+        down: function () {
+            if (count === 0) {
+                count = size;
+                return count;
+            }
+            --count;
+            console.log(count);
+        },
+        set: function (n) {
+            if (n > 0 && n <= size) {
+                count = n;
+                console.log(count);
+            } else {
+                console.log('Error!!!')
+            }
+        },
+        reset: function () {
+            console.log(count = 0);
+        }
+    }
 }
 
 var slider = initSlider(10);
