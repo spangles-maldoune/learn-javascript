f9e23e02e74b1e53325d6a2899173f651a68766c
spangles-maldoune <spangles.maldoune@gmail.com>
2020-06-11 11:48:47 +0300

    new changes

--- 13/js/main.js
+++ 13/js/main.js
@@ -4,27 +4,17 @@
 setInterval(function () {
    var inputs = document.querySelectorAll('.inputValue');
    var textarea = document.querySelector('.textArea');
-   var tempCont = '';
+   var tempCont = [];
    var separator = ', ';
 
-   var tempArray = []
    for (var i = 0; i < inputs.length; i++) {
-      if (inputs[i].value !== '') {
-         tempArray.push(inputs[i].value)
+
+      if (inputs[i].value) {
+         tempCont.push(inputs[i].value);
       }
+
    }
 
-   if (tempArray.length === 0) {
-      return;
-   }
-
-   if (tempArray.length > 1) {
-      for (var i = 0; i < tempArray.length - 1; i++) {
-         tempCont += tempArray[i] + separator;
-      }
-   }
-
-   tempCont += tempArray[tempArray.length - 1];
-   textarea.textContent = tempCont;
+   textarea.textContent = tempCont.join(separator);
 
 }, 3000)
