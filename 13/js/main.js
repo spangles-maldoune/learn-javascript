b1bab89854a95ce1a9cef7a518c5d82152674eaf
Konstantin Malov <kossovec@gmail.com>
2020-06-14 19:10:54 +0300

    for Ihor2

--- 13/js/main.js
+++ 13/js/main.js
@@ -7,56 +7,58 @@ var obj = {
 };
 
 function SuperMath() {
+
+   this.obj = {
+      x:0,
+      y:0,
+      znak:""
+   }
+
+
+
    this.valueMass = {
-      '+': obj.x + obj.y,
-      '-': obj.x - obj.y,
-      '*': obj.x * obj.y,
-      '/': obj.x / obj.y,
-      '%': obj.x % obj.y,
+      '+': function (obj) { return obj.x + obj.y},
+      '-': function (obj) { return obj.x - obj.y},
+      '*': function (obj) { return obj.x * obj.y},
+      '/': function (obj) { return obj.x / obj.y},
+      '%': function (obj) { return obj.x % obj.y},
    };
-  
-   this.check = function (obj) { 
+
+   this.check = function (obj) {
+      this.obj = obj;
       var conf = confirm('Произвести действие ' + obj.x + ' ' + obj.znak + ' ' + obj.y + ' ?');
 
       if (conf === true) {
-         this.mathDo();
+         this.mathDo(obj);
       } else {
          this.input();
       }
-      
+
    }
 
    this.input = function () {
-      obj.x = +prompt ('Введите новые данные x:', ' ');
-      obj.y = +prompt('Введите новые данные y:', ' ');
-      obj.znak = prompt('Введите новые данные znak:', ' ');
+      this.obj.x = +prompt ('Введите новые данные x:', ' ');
+      this.obj.y = +prompt('Введите новые данные y:', ' ');
+      this.obj.znak = prompt('Введите новые данные znak:', ' ');
       var _stopIfCheck = true;
 
       while (_stopIfCheck) {
-         
-         if (this.valueMass[obj.znak]) {
-            this.mathDo.call(obj);
+         if (this.valueMass[this.obj.znak]) {
+            var result = this.mathDo(this.valueMass, obj);
             _stopIfCheck = false;
          } else {
-            obj.znak = prompt('Некорректный ввод математического действия. Введите один из следующих знаков: +, -, /, *, %', ' ');
+            this.obj.znak = prompt('Некорректный ввод математического действия. Введите один из следующих знаков: ' + Object.keys( this.valueMass ), ' ');
          }
-         
+
       }
-      
+
    }
 
 }
 
-SuperMath.prototype.mathDo = function() {
-   // var valueMass = {
-   //    '+': obj.x + obj.y,
-   //    '-': obj.x - obj.y,
-   //    '*': obj.x * obj.y,
-   //    '/': obj.x / obj.y,
-   //    '%': obj.x % obj.y,
-   // };
-
-   alert(this.valueMass[obj.znak]);
+SuperMath.prototype.mathDo = function(valueMass, obj) {
+   var operation = valueMass[obj.znak];
+   return operation(obj);
 }
 
 var p = new SuperMath();
