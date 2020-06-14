988bbec4c62ebaea3ea6f6fd4f48b492447e0ecf
spangles-maldoune <61394048+spangles-maldoune@users.noreply.github.com>
2020-06-14 18:32:02 +0300

    Update main.js

--- 13/js/main.js
+++ 13/js/main.js
@@ -1,16 +1,64 @@
 //Есть 3 input.Выводить в textarea содержимое всех полей ввода через запятую.Использовать setInterval.
 
-function inputListen() {
-    var inputs = document.querySelectorAll('.inputValue');
-    var textarea = document.querySelector('.textArea');
-    var tempCont = '';
-    console.log(inputs)
-    for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].value) {
-            tempCont += inputs[i].value + ', ';
-        }
-    }
-    tempCont = tempCont.trim();
-    textarea.textContent = tempCont.charAt(tempCont.length - 1) === ',' ?
-        tempCont.substr(0, tempCont.length - 1) : tempCont
+var obj = {
+   x: 12,
+   y: 3,
+   znak: '/'
+};
+
+function SuperMath() {
+   this.valueMass = {
+      '+': obj.x + obj.y,
+      '-': obj.x - obj.y,
+      '*': obj.x * obj.y,
+      '/': obj.x / obj.y,
+      '%': obj.x % obj.y,
+   };
+  
+   this.check = function (obj) { 
+      var conf = confirm('Произвести действие ' + obj.x + ' ' + obj.znak + ' ' + obj.y + ' ?');
+
+      if (conf === true) {
+         this.mathDo();
+      } else {
+         this.input();
+      }
+      
+   }
+
+   this.input = function () {
+      obj.x = +prompt ('Введите новые данные x:', ' ');
+      obj.y = +prompt('Введите новые данные y:', ' ');
+      obj.znak = prompt('Введите новые данные znak:', ' ');
+      var _stopIfCheck = true;
+
+      while (_stopIfCheck) {
+         
+         if (this.valueMass[obj.znak]) {
+            this.mathDo.call(obj);
+            _stopIfCheck = false;
+         } else {
+            obj.znak = prompt('Некорректный ввод математического действия. Введите один из следующих знаков: +, -, /, *, %', ' ');
+         }
+         
+      }
+      
+   }
+
 }
+
+SuperMath.prototype.mathDo = function() {
+   // var valueMass = {
+   //    '+': obj.x + obj.y,
+   //    '-': obj.x - obj.y,
+   //    '*': obj.x * obj.y,
+   //    '/': obj.x / obj.y,
+   //    '%': obj.x % obj.y,
+   // };
+
+   alert(this.valueMass[obj.znak]);
+}
+
+var p = new SuperMath();
+
+p.check(obj);
