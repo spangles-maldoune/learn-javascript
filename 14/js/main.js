7f91fcf9bbc97b7c1a5de6becc5c048bd41eb0ca
spangles-maldoune <spangles.maldoune@gmail.com>
2020-06-18 14:18:44 +0300

    home_work14

---
+++ 14/js/main.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+
+   var inpRange = document.querySelector('.inpRange');
+   var inpNumb = document.querySelector('.inpNumb');
+   var blockGreen = document.querySelector('.blockGreen');
+   var blockRed = document.querySelector('.blockRed');
+   inpNumb.value = inpRange.value;
+
+
+   function commissionMath(value) {
+      var numVal = +value;
+      var tempComm;
+
+      if (numVal < 20) {
+         tempComm = (numVal / 100) * 2;
+      } else if (numVal >= 20 && numVal < 50) {
+         tempComm = (numVal / 100) * 4;
+      } else if (numVal >= 50 && numVal < 75) {
+         tempComm = (numVal / 100) * 6;
+      } else if (numVal >= 75 && numVal <= 100) {
+         tempComm = (numVal / 100) * 8;
+      }
+      return tempComm;
+
+   }
+
+   inpRange.oninput = function () {
+      inpNumb.value = inpRange.value;
+      blockGreen.style.height = inpNumb.value + 'px';
+      blockRed.style.bottom = inpNumb.value + 'px';
+      var persetn = commissionMath(inpNumb.value);
+      blockRed.style.height = persetn + 'px';
+   }
+
+   inpNumb.oninput = function () {
+      inpRange.value = inpNumb.value;
+      blockGreen.style.height = inpNumb.value + 'px';
+      blockRed.style.bottom = inpNumb.value + 'px';
+      var persetn = commissionMath(inpNumb.value);
+      blockRed.style.height = persetn + 'px'; 
+   }
+
+}
+
+
