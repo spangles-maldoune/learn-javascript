6828142308f97b0be96655a479a3b6b132e501cd
spangles-maldoune <61394048+spangles-maldoune@users.noreply.github.com>
2020-06-29 20:12:19 +0300

    Update main.js

--- DOM/js/main.js
+++ DOM/js/main.js
@@ -67,67 +67,66 @@ window.onload = function () {
 
    var actions = {
       actionReset: function () {
-         
+         console.log('actionReset');
       },
       actionHelp: function () {
-         
+         console.log('actionHelp');  
       },
       actionPrint: function () {
-
+         console.log('actionPrint');
       },
       actionDevTools: function () {
-
+         console.log('actionDevTools');
       }
-
    }
 
    function ContextMenuComponent () {
       
    }
 
-   ContextMenuComponent.prototype.makeContextMenu = function () {
+   ContextMenuComponent.prototype.makeContextMenu = function (data, actions) {
       var ul = document.createElement('ul');
       ul.classList.add('right-click-menu');
       for (var i = 0; i < data.items.length; i++) {
-         var li = document.createElement('li');
-         li.innerText = data.items[i].title;
-
+         var li = liCreator(data.items[i].title);
+         li.addEventListener('click', actions[data.items[i].actionMethod]);
          ul.append(li);
       }
       document.body.append(ul);
-   
+      this.contextMenu = ul;
    }
 
-   ContextMenuComponent.prototype.rightClickMenu = function () {
-     
-
+   ContextMenuComponent.prototype.getContextMenu = function (params) {
+      return this.contextMenu;
    }
 
-   var contextMenu = new ContextMenuComponent();
-   contextMenu.makeContextMenu();
+   function liCreator(title) {
+      var li = document.createElement('li');
+      li.innerText = title;
+      return li;
+   }
 
+   ContextMenuComponent.prototype.eventActions = function () {
 
-   var contextMenu = document.querySelector('.right-click-menu');
+      document.addEventListener('contextmenu', function rightClick(contextMenu) {
+         event.preventDefault();
+         contextMenu.style.top = event.clientY + 'px';
+         contextMenu.style.left = event.clientX + 'px';
+         contextMenu.classList.add('active');
+      }.bind(null, contextMenu));
 
-   document.addEventListener('contextmenu', function rightClick(event) {
-      event.preventDefault();
-      contextMenu.style.top = event.clientY + 'px';
-      contextMenu.style.left = event.clientX + 'px';
-      contextMenu.classList.add('active');
-   });
-
-   document.addEventListener('click', function click(event) {
-      contextMenu.classList.remove('active');
+      document.addEventListener('click', function click(event) {
+         contextMenu.classList.remove('active');
+      }.bind(null, contextMenu));
       
-   });
+   }
 
-   contextMenu.addEventListener('click', function (event) {
-      event.stopPropagation();
-   });
+   var createMenu = new ContextMenuComponent();
+   createMenu.makeContextMenu(data, actions);
+   var contextMenu = createMenu.getContextMenu();
+   createMenu.eventActions();
 
    
 }
-
-
 
 
