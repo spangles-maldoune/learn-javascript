77e1a068875c400e1025712d770a7c3ef7f41e5e
spangles-maldoune <spangles.maldoune@gmail.com>
2020-06-30 13:50:05 +0300

    make edits

--- 15/js/practice.js
+++ 15/js/practice.js
@@ -1,10 +1,17 @@
+// Реализовать контекстное меню.Список хранить в памяти.При клике правой кнопкой мышии:
+// появляется контекстное меню в месте нажатяи кнопкой мыши(дизайн и принцип работы сделать точно такой же как работает в браузере)
+// список контекстного меню на ваше усмотрение
+// меню пропадает при нажатии в любом месте окна за границами контекстного меню
+// Хранить в списке action - название функции которая будет выполнятся при нажатии на пункт меню из задания №1(принцип работы как делали на практике)
+
+
 window.onload = function () {
 
    var data = {
       name: 'contextMenu',
       items: [
          {
-            title:'перезагрузить',
+            title: 'перезагрузить',
             actionMethod: 'actionReset'
          },
          {
@@ -27,7 +34,7 @@ window.onload = function () {
          console.log('actionReset');
       },
       actionHelp: function () {
-         console.log('actionHelp');  
+         console.log('actionHelp');
       },
       actionPrint: function () {
          console.log('actionPrint');
@@ -37,8 +44,8 @@ window.onload = function () {
       }
    }
 
-   function ContextMenuComponent () {
-      
+   function ContextMenuComponent() {
+
    }
 
    ContextMenuComponent.prototype.makeContextMenu = function (data, actions) {
@@ -50,11 +57,7 @@ window.onload = function () {
          ul.append(li);
       }
       document.body.append(ul);
-      this.contextMenu = ul;
-   }
-
-   ContextMenuComponent.prototype.getContextMenu = function () {
-      return this.contextMenu;
+      return this._contextMenu = ul;
    }
 
    function liCreator(title) {
@@ -63,25 +66,23 @@ window.onload = function () {
       return li;
    }
 
-   ContextMenuComponent.prototype.eventActions = function () {
+   ContextMenuComponent.prototype.addEventListeners = function () {
 
-      document.addEventListener('contextmenu', function rightClick(contextMenu) {
+      document.addEventListener('contextmenu', function rightClick(contextMenu, event) {
          event.preventDefault();
          contextMenu.style.top = event.clientY + 'px';
          contextMenu.style.left = event.clientX + 'px';
          contextMenu.classList.add('active');
-      }.bind(null, contextMenu));
+      }.bind(null, this._contextMenu));
 
-      document.addEventListener('click', function click() {
+      document.addEventListener('click', function click(contextMenu) {
          contextMenu.classList.remove('active');
-      }.bind(null, contextMenu));
-      
+      }.bind(null, this._contextMenu));
+
    }
 
    var createMenu = new ContextMenuComponent();
    createMenu.makeContextMenu(data, actions);
-   var contextMenu = createMenu.getContextMenu();
-   createMenu.eventActions();
+   createMenu.addEventListeners();
 
-   
 }
