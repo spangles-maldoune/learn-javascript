cd7de606b246b6d6747984643082aff33a2d8160
spangles-maldoune <61394048+spangles-maldoune@users.noreply.github.com>
2020-07-08 20:20:42 +0300

    Update main.js

--- 13/js/main.js
+++ 13/js/main.js
@@ -64,3 +64,62 @@ SuperMath.prototype.mathDo = function(valueMass, obj) {
 var p = new SuperMath();
 
 p.check(obj);
+
+
+
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
+
+app.use(function (req, res, next) {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+   res.setHeader('Access-Control-Allow-Credentials', true);
+   next();
+});
+
+const port = 3003;
+
+app.listen(port, function () {
+   console.log(`Example app listening on port http://localhost:${port}/`);
+});
+
+//------------------------------------------- end config ------------------------------------------
+
+// app.get('/set-user-info', function (req, res) {
+//    console.log('hello path /set-user-info');
+//    res.send('Hello Node!');
+// });
+let tempArray = [];
+
+app.post('/set-user-info', function (req, res) {
+   const jsonString = JSON.stringify(req.body);
+   const parseObjectFromServer = JSON.parse(jsonString);
+   tempArray.push(parseObjectFromServer);
+
+
+   let userStringData = fs.readFileSync('./users.json', 'utf8'); 
+   const userDataArray = JSON.parse(userStringData);
+   
+   let secretKey = userDataArray
+   .map((userData) => userData.secretKey)
+   .find((secretKey) => secretKey === parseObjectFromServer.secretKey);
+
+   fs.writeFileSync('./users.json', JSON.stringify(tempArray));
+
+   if (secretKey) {
+      res.status(301).send('Пользватель уже существует');
+   } else {
+      res.status(200).send('Ok');
+   }
+      
+
+   res.send(tempArray);
+})
