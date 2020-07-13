da1765e74e823751e7adb9d6455ec5be7e31b957
spangles-maldoune <spangles.maldoune@gmail.com>
2020-07-13 17:42:07 +0300

    home_work21

---
+++ 21/js/practice.js
@@ -0,0 +1,64 @@
+// Используя цепочку Promise согласно таблицы http://prntscr.com/oxzs7j. 
+// Организовать вывод в консоль такой порядок цифр " 0 1 3 6 8 9 12 ", " 0 2 3 6 7 9 12". 
+// Где 0 - это значение которое выводится в сallback - ф-ии которая передается в Promise.
+
+
+window.onload = function () {
+   const isNumChainingOut = new Promise((resolve, reject) => {
+      console.log(0);
+      reject();
+   });
+
+   isNumChainingOut
+      .then(() => {
+         console.log(1);
+      },
+         () => {
+            console.log(2);
+            const flag = true;
+            return flag;
+         })
+
+      .then((flag) => {
+         console.log(3);
+         return Promise.reject(flag);
+      },
+         () => {
+            console.log(4);
+         })
+
+      .then(() => {
+         console.log(5);
+      },
+         (flag) => {
+            if (flag) {
+               console.log(6);
+            } else {
+               console.log(6);
+               return Promise.reject();
+            }
+         })
+
+      .then(() => {
+         console.log(7);
+      },
+         () => {
+            console.log(8);
+         })
+
+      .then(() => {
+         console.log(9);
+         return Promise.reject();
+      },
+         () => {
+            console.log(10);
+         })
+
+      .then(() => {
+         console.log(11);
+      },
+         () => {
+            console.log(12);
+         });
+
+}
