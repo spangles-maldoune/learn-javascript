abbf2b1edae4b235700054af1b9aa76644409489
spangles-maldoune <61394048+spangles-maldoune@users.noreply.github.com>
2020-07-26 22:36:22 +0300

    Update my_solution.js

--- new/js/my_solution.js
+++ new/js/my_solution.js
@@ -1,11 +1,60 @@
-(function () {
-    let array = [1, 2, 3, 4, 5, 6]
-    for (let i = 0; i < array.length - 1; i += 2) {
-        let indexOne = i
-        let indexTwo = i + 1
-        let temp = array[indexOne]
-        array[indexOne] = array[indexTwo]
-        array[indexTwo] = temp
-    }
-    console.log(array)
-}())
+Vue.component('task-manager', {
+   data() {
+      //модель для представления компонента
+      return {
+         cssClassList: ['navigation', 'header-navigation'],
+         taskItems: ['Task item 1', 'Task item 2', 'Task item 3', 'Task item 4']
+      }
+   },
+   methods: {
+      //разделение состояния компонента от его поведения
+      sendTask() {
+         console.log('sendTask')
+      }
+   },
+   //уникальный шаблон для визуализации компонента, использующий модель  поведение объекта
+   template: `
+      <ol :class="cssClassList">
+         <task-list-item :taskText="item" v-for="item in taskItems"></task-list-item>
+         
+      </ol>
+      <send-button @hello="sendTask"></send-button>
+   `
+});
+
+Vue.component('task-list-item', {
+   props:['taskText'],
+   data() {
+      return {
+         
+      }
+   },
+   methods: {
+     
+   },
+   template: `
+      <li class="task-item">{{taskText}}</li>
+   `
+})
+
+Vue.component('send-button', {
+   data() {
+      return {
+         buttonName: 'transfer'
+      }
+   },
+   methods: {
+      onClick(event) {
+         this.$emit('hello');
+      }
+   },
+   template: `
+      <button @click="onClick" class="send-btn">{{buttonName}}</button>
+   `
+})
+
+
+const vue = new Vue({
+   el: '#app'
+});
+
