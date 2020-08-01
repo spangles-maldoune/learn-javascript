4615eaca3027c035396085fe1b3de6043f987105
spangles-maldoune <61394048+spangles-maldoune@users.noreply.github.com>
2020-08-01 11:32:54 +0300

    Update my_solution.js

--- new/js/my_solution.js
+++ new/js/my_solution.js
@@ -5,17 +5,17 @@ Vue.component('diagrams', {
             {
                name: 'diagramRed',
                color: 'red',
-               heightValue: '200'
+               heightValue: '100'
             },
             {
                name: 'diagramGreen',
                color: 'green',
-               heightValue: '125'
+               heightValue: '100'
             },
             {
                name: 'diagramYellow',
                color: 'yellow',
-               heightValue: '200'
+               heightValue: '100'
             },
             {
                name: 'diagramPink',
@@ -25,40 +25,50 @@ Vue.component('diagrams', {
             {
                name: 'diagramOrange',
                color: 'orange',
-               heightValue: '200'
+               heightValue: '100'
             },
             {
                name: 'diagramBlue',
                color: 'blue',
-               heightValue: '200'
+               heightValue: '100'
             },
             {
                name: 'diagramBlack',
                color: 'black',
-               heightValue: '200'
+               heightValue: '100'
             }
          ],
+         
          
       }
    },
    methods: {
       getHeightValue(heightValue, name) {
          const itemsObj = this.diagItemObj;
-         console.log(heightValue)
          const item = itemsObj.find(item => item.name === name);
          item.heightValue = heightValue;
-         console.log(itemsObj);
+         this.setLocalDiagram(item.name, heightValue)
+      },
+      setLocalDiagram(key, heightValue) {
+         localStorage[key] = heightValue;
+      },
+      sortByHeight() {
+         this.diagItemObj.sort((a, b) => { return a.heightValue - b.heightValue })
+         console.log('sort from head');
       }
    },
+   mounted () {
+      this.diagItemObj.forEach(value => value.heightValue = localStorage[value.name])
+   },
    template:`
-   <div class="global-style">
-      <block-range
-         :itemObject="item" 
-         v-for="item in diagItemObj"
-         @sendHeightValue="getHeightValue">
-   
-      </block-range>
-   </div>
+      <div class="global-style">
+         <block-range
+            :itemObject="item" 
+            v-for="item in diagItemObj"
+            @sendHeightValue="getHeightValue">
+         </block-range>
+         <sort-btn @sendSort="sortByHeight"></sort-btn>
+      </div>
    `
 });
 
@@ -74,14 +84,15 @@ Vue.component('block-range', {
    methods: {
       getHeightValue(heightValue, name) {
          this.$emit('sendHeightValue', heightValue, name);
+         
       }
 
    },
    template: `
-   <div>
-      <diagram-item :objData="itemObject"></diagram-item>
-      <input-item :objData="itemObject" @sendHeightValue="getHeightValue"></input-item>
-   </div>
+      <div>
+         <diagram-item :objData="itemObject"></diagram-item>
+         <input-item :objData="itemObject" @sendHeightValue="getHeightValue"></input-item>
+      </div>
      
    `
 })
@@ -91,20 +102,24 @@ Vue.component('diagram-item', {
    props: ['objData'],
    data() {
       return {
-         customStyle: {
-            'background-color': this.objData.color,
-            'height': this.objData.heightValue + 'px',
-         }
-
+        
+      }
+   },
+   computed: {
+      bgColor: function () {
+         return this.objData.color;
+      },
+      heightVal: function () {
+         return this.objData.heightValue +'px';
       }
    },
    methods: {
-      
+     
 
    },
    template: `
-   <div class="item-style" :style="customStyle"></div>
-   
+      <div class="item-style" :style="{backgroundColor: bgColor, height: heightVal}"></div>
+
    `
 })
 
@@ -126,14 +141,32 @@ Vue.component('input-item', {
          const input = event.target;
          const name = this.name;
          const presentHeightValue = input.value;
-         console.log(presentHeightValue, 'presentHeightValue');
-      
          this.$emit('sendHeightValue', presentHeightValue, name);
       }
    },
    template: `
-   <input :type="type" :name="name" :min="value.min" :max="value.max" @change="changeValue">
+      <input :type="type" :name="name" :min="value.min" :max="value.max" @change="changeValue">
    `
+})
+
+Vue.component ('sort-btn', {
+   data () {
+      return {
+         btnName: 'sort ascending'
+
+      }
+
+   },
+   methods: {
+      sortOnClick () {
+         this.$emit('sendSort');
+      }
+
+   },
+   template: `
+      <button @click="sortOnClick">{{btnName}}</button>
+   `
+
 })
 
 
